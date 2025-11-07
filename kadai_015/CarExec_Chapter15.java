1312f637931e16758101c8a1b9bffe22922ec801
SchkJP <hiimsachika@gmail.com>
2025-11-07 16:51:21 +0900

    修正

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -5,7 +5,7 @@ public class CarExec_Chapter15 {
 	public static void main(String[] args) {
 		
 		
-		Car_Chapter15 car1 = new Car_Chapter15 (1,10);
+		Car_Chapter15 car1 = new Car_Chapter15 ();
 		
 		car1.changeGear(3);
 		car1.run();
