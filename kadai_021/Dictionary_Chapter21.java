204ffb1ec510236eba685baff7ac060d635bf4df
SchkJP <hiimsachika@gmail.com>
2025-11-14 11:06:23 +0900

    修正

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -3,36 +3,55 @@ package kadai_021;
 import java.util.HashMap;
 
 public class Dictionary_Chapter21 {
-	public static void main(String[] args) {
+	// フィールドとして辞書(HashMap)を持つ
+	private HashMap<String, String> dictionary;
 	
-	HashMap<String, String> dictionary = new HashMap<String, String>();
-	
-	dictionary.put("apple","りんご");
-	dictionary.put("peach","桃");
-	dictionary.put("banana","バナナ");
-	dictionary.put("lemon","レモン");
-	dictionary.put("pear","梨");
-	dictionary.put("kiwi","キウィ");
-	dictionary.put("strawberry","いちご");
-	dictionary.put("grape","ぶどう");
-	dictionary.put("muscat","マスカット");
-	dictionary.put("cherry","さくらんぼ");
-	
-	
-	String[] searchWords = {"apple", "banana", "grape", "orange"};
-
-		for (String word : searchWords) {
-			if (dictionary.containsKey(word)) {
-				System.out.println(word + "の意味は" + dictionary.get(word));
-			} else {
-				System.out.println(word + "は辞書に存在しません");
-			}
-		}
-	
-	
-	
+	// --- コンストラクタ ---
+	public Dictionary_Chapter21() {
+		dictionary = new HashMap<>(); // ← HashMap を生成（コンストラクタ呼び出し）
+		
+		// 初期データを登録
+		dictionary.put("apple","りんご");
+		dictionary.put("peach","桃");
+		dictionary.put("banana","バナナ");
+		dictionary.put("lemon","レモン");
+		dictionary.put("pear","梨");
+		dictionary.put("kiwi","キウィ");
+		dictionary.put("strawberry","いちご");
+		dictionary.put("grape","ぶどう");
+		dictionary.put("muscat","マスカット");
+		dictionary.put("cherry","さくらんぼ");
+		
 	}
 	
+	// 使うメソッドの例
+	public String lookup(String key) {
+		return dictionary.get(key);
+	}
+	
+	
+	
+	
+//	public static void main(String[] args) {
+//	
+//	HashMap<String, String> dictionary = new HashMap<String, String>();
+//	
+//	
+//	
+//	String[] searchWords = {"apple", "banana", "grape", "orange"};
+//
+//		for (String word : searchWords) {
+//			if (dictionary.containsKey(word)) {
+//				System.out.println(word + "の意味は" + dictionary.get(word));
+//			} else {
+//				System.out.println(word + "は辞書に存在しません");
+//			}
+//		}
+//	
+//	
+//	
+//	}
+//	
 //	public void search() {
 //		for (String word : dictionary.keySet()) {
 //			if (dictionary.containsKey(word)) {
